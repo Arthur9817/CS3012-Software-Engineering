956e4e6d8f0c68ad865e4ecfb106c91d98bb648f
Arthur Sasunts <arthur@Arthurs-MacBook-Pro.local>
2018-09-25 22:38:51 +0100

    updated tests

--- test/lca_test.hs
+++ test/lca_test.hs
@@ -1,2 +1,12 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module lca_test (spec) where
+
+import           lca
+import           Test.Hspec
+import           Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "LCA Function" $ do
+
+    it "returns 'Error' for empty tree" $ do
+      (lca 1 3) ==  "Error" `shouldBe` True
