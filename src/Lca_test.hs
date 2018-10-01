b1517e2626837f457ff6882d4cb44d46ac51f3e6
Arthur Sasunts <arthur@Arthurs-MacBook-Pro.local>
2018-10-01 18:32:24 +0100

    added test

--- src/Lca_test.hs
+++ src/Lca_test.hs
@@ -11,8 +11,11 @@ main =  hspec $ do
     it "returns 1 for LCA 2 and 3" $ do
       (lca_show myTree 2 3) == "1" `shouldBe` True
 
-    it "returns 1 for LCA 4 and 5" $ do
+    it "returns 2 for LCA 4 and 5" $ do
       (lca_show myTree 4 5) == "2" `shouldBe` True
 
-    it "returns 1 for LCA 6 and 7" $ do
+    it "returns 3 for LCA 6 and 7" $ do
       (lca_show myTree 6 7) == "3" `shouldBe` True
+
+    it "returns 1 for LCA 2 and 6" $ do
+      (lca_show myTree 2 6) == "1" `shouldBe` True
