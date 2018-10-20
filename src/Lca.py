331235a0324db4185e59616331503d2a5be68227
Arthur Sasunts <arthur@Arthurs-MacBook-Pro.local>
2018-10-20 17:18:39 +0100

    Added test.

--- src/Lca.py
+++ src/Lca.py
@@ -7,7 +7,6 @@ class Node:
 
 def findLCA(root, n1, n2):
 
-    # Base Case
     if root is None:
         return None
 
@@ -28,4 +27,4 @@ root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
 root.right.left = Node(6)
-root.right.right = Node(7) 
+root.right.right = Node(7)
--- src/Lca_test.py
+++ src/Lca_test.py
@@ -3,6 +3,10 @@ import Lca
 
 class TestLCA(unittest.TestCase):
 
+    def test_node_1_and_3(self):
+        result = Lca.findLCA(Lca.Node(1), 1, 3).key
+        expected = 1
+        self.assertEqual(expected, result)
 
 if __name__ == '__main__':
     unittest.main()
