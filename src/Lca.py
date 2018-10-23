186d2024f692dc25638653bca9773d3eb7e63c3d
Arthur Sasunts <arthur@Arthurs-MacBook-Pro.local>
2018-10-23 18:49:13 +0100

    Changed code.

--- src/Lca.py
+++ src/Lca.py
@@ -1,25 +1,42 @@
 class Node:
 
     def __init__(self, key):
-        self.key = key
+        self.key =  key
         self.left = None
         self.right = None
 
-def findLCA(root, n1, n2):
+def findPath( root, path, k):
 
     if root is None:
+        return False
+
+    path.append(root.key)
+
+    if root.key == k :
+        return True
+
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))):
+        return True
+
+    path.pop()
+    return False
+
+def findLCA(root, n1, n2):
+
+    path1 = []
+    path2 = []
+
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
         return None
 
-    if root.key == n1 or root.key == n2:
-        return root
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        if path1[i] != path2[i]:
+            break
+        i += 1
+    return path1[i-1]
 
-    left_lca = findLCA(root.left, n1, n2)
-    right_lca = findLCA(root.right, n1, n2)
-
-    if left_lca and right_lca:
-        return root
-
-    return left_lca if left_lca is not None else right_lca
 
 root = Node(1)
 root.left = Node(2)
