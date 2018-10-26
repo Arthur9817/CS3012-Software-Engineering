388e7054c88feaf66bbca23bb477ccbf12db7be4
Arthur Sasunts <arthur@Arthurs-MacBook-Pro.local>
2018-10-26 21:02:16 +0100

    added tests.

--- src/Lca_DAG_test.py
+++ src/Lca_DAG_test.py
@@ -13,5 +13,30 @@ class TestDAG(unittest.TestCase):
         result = Lca_DAG.findLCA(Lca_DAG.root, Lca_DAG.root, Lca_DAG.n3)
         self.assertEqual(expected, result)
 
+    def test_lca_2_3(self):
+        expected = 1
+        result = Lca_DAG.findLCA(Lca_DAG.root, Lca_DAG.n2, Lca_DAG.n3)
+        self.assertEqual(expected, result)
+
+    def test_lca_4_6(self):
+        expected = 3
+        result = Lca_DAG.findLCA(Lca_DAG.root, Lca_DAG.n4, Lca_DAG.n6)
+        self.assertEqual(expected, result)
+
+    def test_lca_5_7(self):
+        expected = 2
+        result = Lca_DAG.findLCA(Lca_DAG.root, Lca_DAG.n5, Lca_DAG.n7)
+        self.assertEqual(expected, result)
+
+    def test_lca_6_7(self):
+        expected = 1
+        result = Lca_DAG.findLCA(Lca_DAG.root, Lca_DAG.n6, Lca_DAG.n7)
+        self.assertEqual(expected, result)
+
+    def test_lca_2_6(self):
+        expected = 4
+        result = Lca_DAG.findLCA(Lca_DAG.root, Lca_DAG.n2, Lca_DAG.n6)
+        self.assertEqual(expected, result)
+
 if __name__ == '__main__':
     unittest.main()
